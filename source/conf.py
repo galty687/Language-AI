efa46bfc514e755b243f9f25f0a3e7bee106711e
Zhijun Gao <galty687@gmail.com>
2025-04-08 13:10:13 +0800

    update

--- source/conf.py
+++ source/conf.py
@@ -22,7 +22,7 @@ copyright = 'Zhijun Gao'
 author = '高志军'
 
 # The full version, including alpha/beta/rc tags
-release = '0.5'
+release = '0.6'
 
 
 # -- General configuration ---------------------------------------------------
